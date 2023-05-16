f84cb1dca91da486aa2272f78b9293446ce32728
Oboro <oboro1102@gmail.com>
2023-05-16 17:25:44 +0800

    調整打包網域位址

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Excel-To-Static-Site/',
+  base: '/outerheaven/Excel-To-Static-Site/',
   build: {
     outDir: 'docs'
   }
